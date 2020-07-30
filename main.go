df0c58c3269faf7b8975cb9df7e23bba3411307d
Christopher Tregear <christregear@gmail.com>
2020-07-30 14:20:09 +0100

    Add files via upload

--- main.go
+++ main.go
@@ -1,33 +1,34 @@
-//The purpose of this programme is to show how we can read data in the form of json
-//into a struct.
+//The purpose of this programme is to show how UNSTRUCTURED* data can be decoded into a map.
 package main
 
-import(
+import (
 	"fmt"
 	"encoding/json"
 	"io/ioutil"
 )
 
-type Countries struct {
-	Countries []Country
-}
-
-type Country struct {
-	Name string
-	Capital string
+type FruitBasket struct {
+    Name    string `json: "fname"`
+    Fruit   []string
+    Id      int64 `json:"ref"`
 }
 
 func main(){
-	file, err := ioutil.ReadFile("data.json")
+	
+	file, err := ioutil.ReadFile("test.json")
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	var payload Countries{}
+	var result []map[string] interface{}
 
-	err = json.Unmarshal([]byte(file), &payload)
-	
-	m := payload.(map[string]interface{})
-	fmt.Println(m)
+	err = json.Unmarshal([]byte(file), &result)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for key, value := range result {
+		fmt.Println(key, value)
+	}
 
 }
